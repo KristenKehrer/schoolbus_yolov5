1fe2bc177e414c41046efd0fc404fe94eedea16f
kkehrer <kristenkehrer@gmail.com>
2022-10-12 11:06:35 -0400

    Adding show datagrid script

---
+++ show_datagrid.py
@@ -0,0 +1,8 @@
+"""
+Run this python script to start up the Datagrid webserver to view *.datagrid files.
+# In our case, we should see the "schoolbus-detection.datagrid" file from running datagrid_detect.py
+"""
+
+import datagrid
+
+datagrid.show()
